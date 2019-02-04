6a77073dd7fe9ab02c28cc5bd2667ab1141fcc4e
HectorRDB <hector.rouxdebezieux@berkeley.edu>
2019-02-03 22:51:10 -0800

    Fixed typo in RSEC

--- 6-RSEC.R
+++ 6-RSEC.R
@@ -50,17 +50,16 @@ for (name in names(sce)) {
   print(name)
   Sce <- sce[[name]]
   print(system.time(
-    seAllen <- RSEC(Sce, k0s = seq(5, 50, by = 5), alphas = c(0.1,0.3),
-                    reduceMethod = "zinbwave100",
-                    nReducedDims = c(3, 5, 10, 20, 50, 100),
-                    sequential = sequential, subsample = subsample, minSizes = 1,
-                    betas = c(0.8), clusterFunction = clusterFunction,
-                    ncores = NCORES, isCount = FALSE, dendroReduce = "zinbwave100",
-                    dendroNDims = 100, verbose = TRUE, consensusProportion = 0.7,
-                    subsampleArgs = list(resamp.num = 50, clusterFunction = "kmeans"),
-                    mergeMethod = "adjP", mergeCutoff = 0.1,
-                    mergeLogFCcutoff = 1, random.seed = 23578,
-                    consensusMinSize = 10, run = TRUE)
+    Sce <- RSEC(Sce, k0s = seq(5, 50, by = 5), alphas = c(0.1,0.3),
+                reduceMethod = "zinb100",
+                nReducedDims = c(3, 5, 10, 20, 50, 100),
+                sequential = sequential, subsample = subsample, minSizes = 1,
+                betas = c(0.8), clusterFunction = clusterFunction,
+                ncores = NCORES, isCount = FALSE, dendroReduce = "zinb100",
+                dendroNDims = 100, verbose = TRUE, consensusProportion = 0.7,
+                subsampleArgs = list(resamp.num = 50, clusterFunction = "kmeans"),
+                mergeMethod = "adjP", mergeCutoff = 0.1, mergeLogFCcutoff = 1,
+                random.seed = 23578, consensusMinSize = 10, run = TRUE)
   ))
   sce[[name]] <- Sce
 }
