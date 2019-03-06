6cd9ebefdced4d4b1c3fb993000e11718c2eb12e
HectorRDB <hector.rouxdebezieux@berkeley.edu>
2019-03-05 18:11:14 -0800

    Update NoAllen.sh

--- Scripts/Smart-Seq/NoAllen.sh
+++ Scripts/Smart-Seq/NoAllen.sh
@@ -6,17 +6,19 @@
 #SBATCH --nodes=1
 
 # Cell
-loc="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_cells_MOp_no_allen"
+loc="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_cells_MOp"
+out="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_cells_MOp_no_allen"
 plot="/accounts/projects/epurdom/singlecell/allen/allen40K/Pipeline_Brain/Figures/Smart-Seq/SMARTer_cells_MOp_no_allen"
 
 Rscript --vanilla --verbose  7-ConsensusClustering.R -n 32 -a FALSE\
-        -l $loc -o $loc -p $plot > 7ab.out 2>&1
+        -l $loc -o $out -p $plot > 7ab.out 2>&1
 echo "Cell dataset"
 
 # Nuclei
-loc="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_nuclei_MOp_no_allen"
+loc="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_nuclei_MOp"
+out="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_nuclei_MOp_no_allen"
 plot="/accounts/projects/epurdom/singlecell/allen/allen40K/Pipeline_Brain/Figures/Smart-Seq/SMARTer_nuclei_MOp_no_allen"
 
 Rscript --vanilla --verbose  7-ConsensusClustering.R -n 32 -a FALSE\
-        -l $loc -o $loc -p $plot> 7bb.out 2>&1
+        -l $loc -o $out -p $plot> 7bb.out 2>&1
 echo "Nuclei dataset"
