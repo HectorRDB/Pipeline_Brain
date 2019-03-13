ac7d4176cfd66f9685cb61c4b34f53e7810474dd
HectorRDB <hector.rouxdebezieux@berkeley.edu>
2019-03-13 14:29:20 -0700

    Update Memory.R

--- Scripts/Helper/Memory.R
+++ Scripts/Helper/Memory.R
@@ -8,7 +8,8 @@ option_list <- list(
   )
 )
 opt <- parse_args(OptionParser(option_list = option_list))
-library(tidyverse)
+library(dplyr)
+library(tidyr)
 if (!is.na(opt$l)) {
   loc <- opt$l
   cat("The selected dataset is located at", loc, "\n")
