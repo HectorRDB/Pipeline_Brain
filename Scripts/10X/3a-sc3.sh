4f55d24c9fd7e42fc6ce5933bd49e02958998ca9
Hector Roux de Bezieux <hectorrb@br005.pvt.bridges.psc.edu>
2019-03-20 13:01:12 -0400

    10x-nuclei pictures

--- Scripts/10X/3a-sc3.sh
+++ Scripts/10X/3a-sc3.sh
@@ -2,7 +2,7 @@
 #SBATCH --mail-user=hector.rouxdebezieux@berkeley.edu
 #SBATCH --mail-type=ALL
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=1
 #SBATCH -p LM
 #SBATCH --mem=3000GB
 #SBATCH -t 4-00:00:00
@@ -14,4 +14,4 @@ out="/pylon5/ib5phhp/hectorrb/ProcessedData/10x_nuclei_MOp_sc3.rds"
 MEMORYFILE="3a-memoryLogger.txt"
 
 while true; do free -h >> $MEMORYFILE; sleep 30; done & Rscript \
-  --no-save --verbose  3-sc3.R -n 6 -l $loc -o $out> 3a.out 2>&1
+  --no-save --verbose  3-sc3.R -n 1 -l $loc -o $out> 3a.out 2>&1
