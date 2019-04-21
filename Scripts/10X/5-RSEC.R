ef12183d168546cd6ea679a31f41cbea37c93377
HectorRDB <hector.rouxdebezieux@berkeley.edu>
2019-04-20 17:32:47 -0700

    Rsec broken down into components

--- Scripts/10X/5-RSEC.R
+++ Scripts/10X/5-RSEC.R
@@ -45,27 +45,58 @@ sce <- readRDS(file = loc)
 
 ##---- run RSEC
 sequential <- FALSE
-subsample <- T
+subsample <- TRUE
 clusterFunction <- "pam"
 NCORES <- as.numeric(opt$n)
 reduceMeth <- reducedDimNames(sce)
 
-cat("Starting RSEC\n")
+cat("Cluster Many\n")
 
 print(system.time(
-  sce <- RSEC(sce, k0s = seq(10, 50, by = 5), alphas = c(0.1, 0.3),
-              reduceMethod = reduceMeth, sequential = sequential,
-              subsample = subsample, minSizes = 1, betas = c(0.8), 
-              clusterFunction = clusterFunction, ncores = NCORES, run = TRUE,
-              isCount = FALSE, dendroReduce = reduceMeth[length(reduceMeth)],
-              dendroNDims = 50, consensusProportion = 0.7, verbose = TRUE,
-              random.seed = 23578, mergeMethod = "adjP", mergeCutoff = 0.05,
-              subsampleArgs = list(resamp.num = 50, clusterFunction = "kmeans"),
-              mergeLogFCcutoff = 1, consensusMinSize = 10)
+  sce <- clusterMany(sce,
+                     ks = seq(10, 50, by = 5),
+                     clusterFunction = clusterFunction,
+                     reduceMethod = reduceMeth,
+                     alphas = c(0.1, 0.3),
+                     sequential = sequential,
+                     betas = c(0.8),
+                     minSizes = 1,
+                     isCount = TRUE,
+                     subsampleArgs = list(resamp.num = 50,
+                                          clusterFunction = "kmeans"),
+                     random.seed = 23578,
+                     run = TRUE,
+                     ncores = NCORES)
 ))
+saveRDS(sce, file = output)
+
+cat("make Consensus\n")
+print(system.time(
+  sce <- makeConsensus(sce,
+                       minSize = 10,
+                       proportion = 0.7)
+))
+saveRDS(sce, file = output)
+
+cat("make Dendogram\n")
+print(system.time(
+  sce <- makeDendrogram(sce,
+                        reduceMethod = reduceMeth[length(reduceMeth)],
+                        nDims = 50)
+))
+saveRDS(sce, file = output)
+
+cat("Merge Clusters\n")
+print(system.time(
+  sce <- mergeClusters(sce,
+                       mergeMethod = "adjP",
+                       cutoff = 0.05,
+                       logFCcutoff = 1)
+))
+
+saveRDS(sce, file = output)
 
 pdf(file = opt$p)
 plotClusters(sce)
 dev.off()
 
-saveRDS(sce, file = output)
