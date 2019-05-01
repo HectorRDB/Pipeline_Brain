9e190b2ed5cc6fa6b52a5b0d769db4992d2ecd55
Elizabeth Purdom <singlecell@beren.Berkeley.EDU>
2019-04-30 21:32:07 -0700

    Run new Consensus on Smart-Seq cells with allen data

--- Scripts/Smart-Seq/6-monocle.sh
+++ Scripts/Smart-Seq/6-monocle.sh
@@ -2,11 +2,11 @@
 #SBATCH --mail-user=hector.rouxdebezieux@berkeley.edu
 #SBATCH --mail-type=ALL
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=1
 #SBATCH --nodes=1
-MEMORYFILE="6a-memoryLogger.txt"
-loc="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_cells_MOp_zinbWs.rds"
-out="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_cells_MOp_monocle.rds"
+MEMORYFILE="6b-memoryLogger.txt"
+loc="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_nuclei_MOp_zinbWs.rds"
+out="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_nuclei_MOp_monocle.rds"
 
 while true; do free -h >> $MEMORYFILE; sleep 15; done & \
-Rscript --vanilla --verbose 6-monocle.R -l $loc -o $out> 6a.out 2>&1
+Rscript --vanilla --verbose 6-monocle.R -l $loc -o $out> 6b.out 2>&1
--- Scripts/Smart-Seq/7-ConsensusClustering.sh
+++ Scripts/Smart-Seq/7-ConsensusClustering.sh
@@ -4,9 +4,9 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
 #SBATCH --nodes=1
-MEMORYFILE="7a_memoryLogger.txt"
-loc="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_cells_MOp"
-out="/accounts/projects/epurdom/singlecell/allen/allen40K/Pipeline_Brain/data/Smart-Seq/SMARTer_cells_MOp_mergers.rds"
+MEMORYFILE="7b-memoryLogger.txt"
+loc="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_nuclei_MOp"
+out="/accounts/projects/epurdom/singlecell/allen/allen40K/Pipeline_Brain/data/Smart-Seq/SMARTer_nuclei_MOp"
 
 while true; do free -h >> $MEMORYFILE; sleep 15; done & \
-Rscript --vanilla --verbose  7-ConsensusClustering.R -n 10 -l $loc -o $loc > 7a.out 2>&1
+Rscript --vanilla --verbose  7-ConsensusClustering.R -n 10 -l $loc -o $out > 7b.out 2>&1
