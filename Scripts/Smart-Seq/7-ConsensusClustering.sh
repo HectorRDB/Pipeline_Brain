f52f761547545fc8cd60b7f8b8c16ae95605e3c8
HectorRDB <hector.rouxdebezieux@berkeley.edu>
2019-04-30 19:49:20 -0700

    Update 7-ConsensusClustering.sh

--- Scripts/Smart-Seq/7-ConsensusClustering.sh
+++ Scripts/Smart-Seq/7-ConsensusClustering.sh
@@ -6,6 +6,7 @@
 #SBATCH --nodes=1
 MEMORYFILE="7a_memoryLogger.txt"
 loc="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_cells_MOp"
+out="/accounts/projects/epurdom/singlecell/allen/allen40K/Pipeline_Brain/data/Smart-Seq/SMARTer_cells_MOp_mergers.rds"
 
 while true; do free -h >> $MEMORYFILE; sleep 15; done & \
 Rscript --vanilla --verbose  7-ConsensusClustering.R -n 10 -l $loc -o $loc > 7a.out 2>&1
