b4e816382b937016deb969dd944e060587b30504
Hector <hector.rouxdebezieux@berkeley.edu>
2019-05-20 14:46:16 +0200

    Update 6-monocle.R

--- Scripts/Smart-Seq/6-monocle.R
+++ Scripts/Smart-Seq/6-monocle.R
@@ -54,7 +54,7 @@ sce <- estimateDispersions(sce)
 sce@normalized_data_projection <- zinbW
 sce@assayData$exprs <- sce@auxOrderingData$normalize_expr_data <- t(zinbW[,1:2])
 sce@featureData <- new("AnnotatedDataFrame",
-                       data = data.frame(gene_short_name = 1:2))
+                       data = data.frame(gene_short_name = colnames(zinbW)[1:2]))
 
 print("Doing the reduced dimension")
 sce <- reduceDimension(sce,
