0f7e622b94e35c5a7c8e16b8aa727234f6afbddd
Hector <hector.rouxdebezieux@berkeley.edu>
2019-05-21 14:32:30 +0200

    here we go again

--- Scripts/10X/6a-monocle.sh
+++ Scripts/10X/6a-monocle.sh
@@ -13,15 +13,10 @@ out="/pylon5/ib5phhp/hectorrb/ProcessedData/10x_nuclei_MOp_monocle.rds"
 module unload intel
 module load anaconda3
 
-# Create the MEMORYFILE file
 timestamp=$(date +"%m-%d-%H:%M")
 MEMORYFILE=${timestamp}_6a-monocle_n-1.txt
 
-# Add the first few lines for analytic purposes.
-# The name variable can also be defined gloably by modifyinh the .bashrc file
-NAME=Hector
 echo $NAME > $MEMORYFILE
-# Replace with your own variables. This is cpus-per-tasks partition mem
 echo 1 "LM" 1000GB >> $MEMORYFILE
 TIMELAPSES=30
 echo $TIMELAPSES >> $MEMORYFILE
@@ -30,8 +25,6 @@ echo $TIMELAPSES >> $MEMORYFILE
 # This can be more than one line
 source activate monocle_env
 while true; do free -h >> $MEMORYFILE; sleep $TIMELAPSES; done & \
-  R CMD BATCH --vanilla --verbose 6-monocle.R 
+  R CMD BATCH  6a-monocle.R 6a.out
 
-# This can also be stored globally
-logStorage=/pylon5/ib5phhp/shared/improved-happiness/xsedelogs
 cp $MEMORYFILE ${logStorage}/$MEMORYFILE
