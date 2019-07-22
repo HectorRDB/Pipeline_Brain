6835f8e9b33521c3b1dcacaff387727a75564ad2
Hector <hector.rouxdebezieux@berkeley.edu>
2019-07-22 09:33:58 +0200

    Update 7-ConsensusClustering.R

--- Scripts/10X/7-ConsensusClustering.R
+++ Scripts/10X/7-ConsensusClustering.R
@@ -47,17 +47,25 @@ library(merger)
 
 # Load Data and clean seurat ----
 # Load sc3  and allen clustering results
+print("Loading sc3")
 sc3 <- readRDS(paste0(loc, "_sc3.rds"))
 allen <- colData(sc3)[, "allenClusters"]
 sc3 <- colData(sc3)[, "sc3_100_clusters"]
+print(sum(is.na(sc3)))
+print("Allen")
+print(sum(is.na(allen)))
 
 # Load Monocle clustering results
+print("Loading Monocle")
 Monocle <- readRDS(paste0(loc, "_monocle2.rds"))
 Monocle <- Monocle[names(sc3)]
+print(sum(is.na(Monocle)))
 
 # Load Seurat
+print("Loading Seurat")
 seurat <- readRDS(paste0(loc, "_seurat.rds"))
 seurat <- seurat[, "1.2,30"]
+print(sum(is.na(seurat)))
 
 # Load Allen
 # Get the final clustering labels
