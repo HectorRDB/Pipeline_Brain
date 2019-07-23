9756bc446b3a8c71026ed1ffe2284cc9978605ec
Hector <hector.rouxdebezieux@berkeley.edu>
2019-07-23 15:15:31 +0200

    no allen

--- Scripts/10X/7-ConsensusClustering.sh
+++ Scripts/10X/7-ConsensusClustering.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
 #SBATCH -p LM
-#SBATCH --mem=1500GB
+#SBATCH --mem=1000GB
 #SBATCH -t 4-00:00:00
 #SBATCH --nodes=1
 
@@ -21,26 +21,16 @@ echo $TIMELAPSES >> $MEMORYFILE
 echo "Nuclei dataset"
 loc="/pylon5/ib5phhp/hectorrb/ProcessedData/10x_nuclei_MOp"
 out="/home/hectorrb/Pipeline_Brain/data/10X/10x_nuclei_MOp"
-echo "... No allen"
-while true; do free -h >> $MEMORYFILE; sleep $TIMELAPSES; done & \
-      Rscript --no-save --verbose  7-ConsensusClustering.R -n 10 -l $loc \
-      -o $out -a FALSE > 7bb.out 2>&1
-echo "... Allen"
-while true; do free -h >> $MEMORYFILE; sleep $TIMELAPSES; done & \
-      Rscript --no-save --verbose  7-ConsensusClustering.R -n 10 -l $loc \
-      -o $out -a TRUE > 7ba.out 2>&1
+# while true; do free -h >> $MEMORYFILE; sleep $TIMELAPSES; done & \
+#       Rscript --no-save --verbose  7-ConsensusClustering.R -n 10 -l $loc \
+#       -o $out -a FALSE > 7bb.out 2>&1
 
 echo "Cells dataset"
 loc="/pylon5/ib5phhp/hectorrb/ProcessedData/10x_cells_MOp"
 out="/home/hectorrb/Pipeline_Brain/data/10X/10x_cells_MOp"
-echo "... No allen"
 while true; do free -h >> $MEMORYFILE; sleep $TIMELAPSES; done & \
       Rscript --no-save --verbose  7-ConsensusClustering.R -n 10 -l $loc \
       -o $out -a FALSE > 7ab.out 2>&1
-echo "... Allen"
-while true; do free -h >> $MEMORYFILE; sleep $TIMELAPSES; done & \
-      Rscript --no-save --verbose  7-ConsensusClustering.R -n 10 -l $loc \
-      -o $out -a TRUE > 7aa.out 2>&1
 
 logStorage=/pylon5/ib5phhp/shared/improved-happiness/xsede/xsedelogs
 cp $MEMORYFILE ${logStorage}/$MEMORYFILE
