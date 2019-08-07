3fc2023aba33bbd27148d1d32c42405aaaec5500
Hector <hector.rouxdebezieux@berkeley.edu>
2019-08-07 17:36:41 +0200

    Update zinb_tsne_scf.sh

--- Scripts/Helper/zinb_tsne_scf.sh
+++ Scripts/Helper/zinb_tsne_scf.sh
@@ -7,8 +7,8 @@
 
 loc="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_cells_MOp_zinbWs.rds"
 out="/accounts/projects/epurdom/singlecell/allen/allen40K/Pipeline_Brain/data/Smart-Seq/SMARTer_cells_MOp_tnse.csv"
-Rscript --vanilla --verbose zinb_tsne.R $loc -o $out -K 30 > cells.out 2>&1
+Rscript --vanilla --verbose zinb_tsne.R -l $loc -o $out -K 30 > cells.out 2>&1
 
 loc="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_nuclei_MOp_zinbWs.rds"
 out="/accounts/projects/epurdom/singlecell/allen/allen40K/Pipeline_Brain/data/Smart-Seq/SMARTer_nuclei_MOp_tnse.csv"
-Rscript --vanilla --verbose zinb_tsne.R $loc -o $out -K 14 > nuclei.out 2>&1
+Rscript --vanilla --verbose zinb_tsne.R -l $loc -o $out -K 14 > nuclei.out 2>&1
