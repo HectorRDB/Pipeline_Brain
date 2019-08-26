587d73dc3f5fb0290f4cf46e81153e81ce464e8b
Hector <hector.rouxdebezieux@berkeley.edu>
2019-08-26 16:19:57 -0700

    singleSeurat

---
+++ Scripts/singleMerge/singleSeurat.R
@@ -0,0 +1,66 @@
+suppressWarnings(library(optparse))
+
+# Arguments for R Script ----
+option_list <- list(
+  make_option(c("-o", "--output"),
+              action = "store", default = NA, type = "character",
+              help = "Where to store the output"
+  ),
+  make_option(c("-l", "--location"),
+              action = "store", default = NA, type = "character",
+              help = "The location of the data"
+  )
+)
+
+opt <- parse_args(OptionParser(option_list = option_list))
+
+if (!is.na(opt$l)) {
+  loc <- opt$l
+  cat("The selected dataset is located at", loc)
+} else {
+  stop("Missing l argument")
+}
+
+if (!is.na(opt$o)) {
+  output <- opt$o
+} else {
+  stop("Missing o argument")
+}
+
+# Load data ----
+library(Seurat)
+library(dplyr)
+library(stringr)
+library(SummarizedExperiment)
+
+sce <- readRDS(file = loc)
+
+clusterMatrixs <- list()
+# Setup ----
+sSeurat <- CreateSeuratObject(raw.data = assays(sce)$counts, project = 'allen40K')
+sSeurat <- NormalizeData(object = sSeurat, normalization.method = "LogNormalize")
+sSeurat <- FindVariableGenes(object = sSeurat, mean.function = ExpMean,
+                             dispersion.function = LogVMR, do.plot = T)
+sSeurat <- ScaleData(object = sSeurat, vars.to.regress = "nUMI")
+sSeurat <- RunPCA(object = sSeurat, pc.genes = sSeurat@var.genes, 
+                  do.print = TRUE, pcs.compute = 50, pcs.print = 1:5,
+                  genes.print = 5)
+  
+# Run clustering ----
+clusterMatrix <- NULL
+for (RESOLUTION in seq(from = 0.3, to = 1.7, by = .1)) {
+  print(RESOLUTION)
+  for (K.PARAM in c(30, 50, 100)) {
+    print(paste0("...", K.PARAM))
+    sSeurat_star <- FindClusters(object = sSeurat, reduction.type = "pca",
+                                 dims.use = 1:50, resolution = RESOLUTION,
+                                 print.output = 0, k.param = K.PARAM,
+                                 save.SNN = TRUE)
+    clusterMatrix <- cbind(clusterMatrix, sSeurat_star@ident)
+    colnames(clusterMatrix)[ncol(clusterMatrix)] <- paste(RESOLUTION, K.PARAM,
+                                                          sep = ",")
+  }
+}
+print(lapply(as.data.frame(clusterMatrix), n_distinct) %>% unlist() %>% table())
+
+write.table(clusterMatrix, file = output)
