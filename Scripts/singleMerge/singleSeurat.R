a7646927197942bc6c5fe07a3e49c5ff45b2543d
Hector <hector.rouxdebezieux@berkeley.edu>
2019-08-26 17:38:32 -0700

    Update singleSeurat.R

--- Scripts/singleMerge/singleSeurat.R
+++ Scripts/singleMerge/singleSeurat.R
@@ -41,7 +41,7 @@ sSeurat <- NormalizeData(object = sSeurat, normalization.method = "LogNormalize"
 sSeurat <- FindVariableFeatures(object = sSeurat, mean.function = ExpMean,
                                 dispersion.function = LogVMR, do.plot = T)
 sSeurat <- ScaleData(object = sSeurat, vars.to.regress = "nCount_RNA")
-sSeurat <- RunPCA(object = sSeurat, ndims.print = 1, pcs.compute = 50)
+sSeurat <- RunPCA(object = sSeurat, ndims.print = 1, pcs.compute = 100)
   
 # Run clustering ----
 clusterMatrix <- NULL
