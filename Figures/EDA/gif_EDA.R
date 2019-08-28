b4cb87e9faa5d2d3be20d35bc2ea34480dab9527
Hector <hector.rouxdebezieux@berkeley.edu>
2019-08-28 16:19:54 -0700

    created gifs

---
+++ Figures/EDA/gif_EDA.R
@@ -0,0 +1,20 @@
+# Inspired by https://ryanpeek.github.io/2016-10-19-animated-gif_maps_in_R/
+library(magick)
+library(purrr)
+library(stringr)
+library(dplyr)
+library(here)
+types <- c("10x_cells", "10x_nuclei", "SMARTer_cells", "SMARTer_nuclei")
+walk(types, function(type){
+  files <- list.files(here("Figures", "EDA")) %>%
+    str_subset(pattern = type)
+  map(files, function(file){
+    K <- str_extract(file, "..\\.png$") %>% str_remove("\\.png$")
+    image_read(here("Figures", "EDA", file)) %>%
+      image_annotate(., text = paste0("K = ", K), gravity = "north", size = 50) %>%
+      return()
+  }) %>%
+    image_join() %>% # joins image
+    image_animate(fps = 1) %>% # animates, can opt for number of loops
+    image_write(here("Figures", "EDA", paste0(type, "_MOp.gif")))
+})
