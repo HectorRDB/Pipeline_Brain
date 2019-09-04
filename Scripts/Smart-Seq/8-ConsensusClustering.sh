2c8097735a37e88498e9e2c30d9f4101f42a21d8
Hector <hector.rouxdebezieux@berkeley.edu>
2019-09-04 16:13:00 -0700

    Update 8-ConsensusClustering.sh

--- Scripts/Smart-Seq/8-ConsensusClustering.sh
+++ Scripts/Smart-Seq/8-ConsensusClustering.sh
@@ -10,11 +10,11 @@ loc="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_cells_MOp"
 out="/accounts/projects/epurdom/singlecell/allen/allen40K/Pipeline_Brain/data/Smart-Seq/SMARTer_cells_MOp"
 plot="/accounts/projects/epurdom/singlecell/allen/allen40K/Pipeline_Brain/Figures/Smart-Seq/SMARTer_cells_MOp"
 while true; do free -h >> $MEMORYFILE; sleep 15; done & \
-Rscript --verbose  7-ConsensusClustering.R -n 10 -l $loc -o $out -p $plot > 7a.out 2>&1
+Rscript --verbose  8-ConsensusClustering.R -n 10 -l $loc -o $out -p $plot > 7a.out 2>&1
 
 MEMORYFILE="8b-memoryLogger.txt"
 loc="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_nuclei_MOp"
 out="/accounts/projects/epurdom/singlecell/allen/allen40K/Pipeline_Brain/data/Smart-Seq/SMARTer_nuclei_MOp"
 plot="/accounts/projects/epurdom/singlecell/allen/allen40K/Pipeline_Brain/Figures/Smart-Seq/SMARTer_nuclei_MOp"
 while true; do free -h >> $MEMORYFILE; sleep 15; done & \
-Rscript --verbose  7-ConsensusClustering.R -n 10 -l $loc -o $out -p $plot > 7b.out 2>&1
+Rscript --verbose  8-ConsensusClustering.R -n 10 -l $loc -o $out -p $plot > 7b.out 2>&1
