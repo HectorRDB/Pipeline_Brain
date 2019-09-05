8212e47614bb7bca6ee086eb718ddf868e5719b4
Hector <hector.rouxdebezieux@berkeley.edu>
2019-09-05 11:45:10 -0700

    Update singleMonocle.sh

--- Replicability/scripts/singleMonocle.sh
+++ Replicability/scripts/singleMonocle.sh
@@ -2,13 +2,13 @@
 #SBATCH --mail-user=hector.rouxdebezieux@berkeley.edu
 #SBATCH --mail-type=ALL
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=32
 #SBATCH --nodes=1
 
-loc="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_nuclei_MOp_zinbWs.rds"
-out="/accounts/projects/epurdom/singlecell/allen/allen40K/Pipeline_Brain/data/singleMerge/SMARTer_nuclei_MOp_singleMonocle.csv"
-Rscript --verbose  singleMonocle.R -l $loc -o $out> nuclei.out 2>&1
+MEMORYFILE=replicability.txt
 
-loc="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_cells_MOp_zinbWs.rds"
-out="/accounts/projects/epurdom/singlecell/allen/allen40K/Pipeline_Brain/data/singleMerge/SMARTer_cells_MOp_singleMonocle.csv"
-Rscript --verbose  singleMonocle.R -l $loc -o $out> cells.out 2>&1
+while true; do free -h >> $MEMORYFILE; sleep 30 ; done & \
+  R CMD BATCH metaneighbor_analysis.R metaneighbor_analysis.Rout
+
+  while true; do free -h >> $MEMORYFILE; sleep 30 ; done & \
+    R CMD BATCH visualization.R visualization.Rout
