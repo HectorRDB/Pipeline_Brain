a213dd53e457a0e62415bed7ba69f1d73ff51700
Hector <hector.rouxdebezieux@berkeley.edu>
2019-09-05 12:55:58 -0700

    Update singleTree.sh

--- Scripts/singleMerge/singleTree.sh
+++ Scripts/singleMerge/singleTree.sh
@@ -7,10 +7,10 @@
 
 # Cells
 loc="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_cells_MOp"
-out="/accounts/projects/epurdom/singlecell/allen/allen40K/Pipeline_Brain/data/singleMerge/SMARTer_cells_MOp_single_Merge.csv"
+out="/accounts/projects/epurdom/singlecell/allen/allen40K/Pipeline_Brain/data/singleMerge/SMARTer_cells_MOp_singleTree.csv"
 Rscript --vanilla --verbose singleTree.R -l $loc -o $out > cells.out 2>&1
 
 # nuclei
 loc="/scratch/users/singlecell/MiniAtlas/data/rds/SMARTer_nuclei_MOp"
-out="/accounts/projects/epurdom/singlecell/allen/allen40K/Pipeline_Brain/data/singleMerge/SMARTer_nuclei_MOp_single_Merge.csv"
+out="/accounts/projects/epurdom/singlecell/allen/allen40K/Pipeline_Brain/data/singleMerge/SMARTer_nuclei_MOp_singleTree.csv"
 Rscript --vanilla --verbose singleTree.R -l $loc -o $out > nuclei.out 2>&1
