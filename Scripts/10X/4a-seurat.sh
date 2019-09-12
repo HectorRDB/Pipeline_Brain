72ad95295736549969c0f693cf41c195760731bf
Hector <hector.rouxdebezieux@berkeley.edu>
2019-09-12 16:16:29 -0700

    Update 4a-seurat.sh

--- Scripts/10X/4a-seurat.sh
+++ Scripts/10X/4a-seurat.sh
@@ -3,15 +3,32 @@
 #SBATCH --mail-type=ALL
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH -p LM
-#SBATCH --mem=1000GB
+#SBATCH -p RM
 #SBATCH -t 4-00:00:00
 #SBATCH --nodes=1
 
-loc="/pylon5/ib5phhp/hectorrb/ProcessedData/10x_cells_MOp_filt.rds"
-out="/pylon5/ib5phhp/hectorrb/ProcessedData/10x_cells_MOp_seurat.rds"
 module load intel/18.4
 module load hdf5
+timestamp=$(date +"%m-%d-%H:%M")
+basename=seurat_cells-nuclei_${timestamp}
+MEMORYFILE=${basename}.txt
 
+echo $NAME > $MEMORYFILE
+echo 1 "RM" >> $MEMORYFILE
+TIMELAPSES=30
+echo $TIMELAPSES >> $MEMORYFILE
 
-Rscript --no-save --verbose  4-seurat.R -l $loc -o $out> 4a.out 2>&1
+echo "Nuclei dataset"
+loc="/pylon5/ib5phhp/hectorrb/ProcessedData/10x_cells_MOp_filt.rds"
+out="/home/hectorrb/Pipeline_Brain/data/singleMethod/10x_cells_MOp_Seurat.csv"
+while true; do free -h >> $MEMORYFILE; sleep $TIMELAPSES; done & \
+      Rscript --no-save --verbose  4-seurat.R -l $loc -o $out> 4a.out 2>&1
+
+echo "Nuclei dataset"
+loc="/pylon5/ib5phhp/hectorrb/ProcessedData/10x_nuclei_MOp_filt.rds"
+out="/home/hectorrb/Pipeline_Brain/data/singleMethod/10x_nuclei_MOp_Seurat.csv"
+while true; do free -h >> $MEMORYFILE; sleep $TIMELAPSES; done & \
+      Rscript --no-save --verbose  4-seurat.R -l $loc -o $out> 4b.out 2>&1
+
+logStorage=/pylon5/ib5phhp/shared/improved-happiness/xsede/xsedelogs
+cp $MEMORYFILE ${logStorage}/$MEMORYFILE
