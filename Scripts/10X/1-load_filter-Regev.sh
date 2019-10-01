7c839eb91aa5f9d90287f6764b8693f72f774898
Hector <hector.rouxdebezieux@berkeley.edu>
2019-10-01 15:24:28 -0700

    Update 1-load_filter-Regev.sh

--- Scripts/10X/1-load_filter-Regev.sh
+++ Scripts/10X/1-load_filter-Regev.sh
@@ -7,10 +7,26 @@
 #SBATCH -t 12:00:00
 #SBATCH --nodes=1
 
+module load intel/18.4
+module load hdf5
+module load gcc
+
+timestamp=$(date +"%Y%m%d-%H%M%S")
+basename=1-load-filter_Regev_${timestamp}
+MEMORYFILE=${basename}.txt
+
+echo $NAME > $MEMORYFILE
+echo 1 "RM" >> $MEMORYFILE
+TIMELAPSES=30
+echo $TIMELAPSES >> $MEMORYFILE
+
+
 loc="/pylon5/ib5phhp/hectorrb/Regev/count_matrix.csv"
 out="/pylon5/ib5phhp/hectorrb/Regev/count_matrix_filt.rds"
 
-module load intel/18.4
-module load hdf5
+while true; do free -h >> $MEMORYFILE; sleep $TIMELAPSES; done & \
+  Rscript --no-save --verbose  1-load_filter.R -l $loc -o $out -c 25 > 1-Regev.out 2>&1
 
-Rscript --no-save --verbose  1-load_filter.R -l $loc -o $out -c 25 > 1b.out 2>&1
+
+logStorage=/pylon5/ib5phhp/shared/improved-happiness/xsede/xsedelogs
+cp $MEMORYFILE ${logStorage}/$MEMORYFILE
