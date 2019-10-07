0848cb8c9e7e6c564950d61b3091a37c01ef5328
Hector <hector.rouxdebezieux@berkeley.edu>
2019-10-07 15:46:51 -0700

    Update 6-Dune.R

--- Scripts/10X/6-Dune.R
+++ Scripts/10X/6-Dune.R
@@ -80,6 +80,7 @@ library(Dune)
 # Load sc3 clustering results
 print("Loading SC3")
 sc3 <- read.csv(paste0(loc, "_SC3.csv"))[, -1]
+colnames(sc3) <- str_remove(colnames(sc3), "^X")
 Names <- sc3$cells
 sc3 <- sc3[, sc3_p] %>% as.numeric()
 
