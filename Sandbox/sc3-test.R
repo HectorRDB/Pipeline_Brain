2f041d91fef845a39bcfad3434dbbaa3a3db866e
Hector <hector.rouxdebezieux@berkeley.edu>
2019-10-17 10:40:14 +0200

    Update sc3-test.R

--- Sandbox/sc3-test.R
+++ Sandbox/sc3-test.R
@@ -1,4 +1,12 @@
 library(SingleCellExperiment)
-df <- readRDS("/pylon5/ib5phhp/hectorrb/ProcessedData/10x_cells_MOp_sc3.rds")
-print(colData(df))
-print(colnames(df)[1:10])
+library(dplyr)
+df <- readRDS("/pylon5/ib5phhp/hectorrb/ProcessedData/10x_nuclei_MOp_sc3.rds")
+df <- data.frame(cells = colnames(df), 
+                 "100" = df$sc3_100_clusters)
+print(dim(df))
+df2 <- read.csv("/home/hectorrb/Pipeline_Brain/data/singleMethod/10x_nuclei_MOp_sc3.csv",
+                row.names = 1)
+df <- inner_join(df, df2)
+print(dim(df))
+print(colnames(df))
+write.csv(df, "/home/hectorrb/Pipeline_Brain/data/singleMethod/10x_nuclei_MOp_sc3.csv")
