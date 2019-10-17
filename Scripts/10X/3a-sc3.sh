5aebe0bb5b516866bbb70c4cb99601282efc5cbc
Hector <hector.rouxdebezieux@berkeley.edu>
2019-10-17 10:43:24 +0200

    Update 3a-sc3.sh

--- Scripts/10X/3a-sc3.sh
+++ Scripts/10X/3a-sc3.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH -p LM
-#SBATCH --mem=1000GB
+#SBATCH --mem=500GB
 #SBATCH -t 7-00:00:00
 #SBATCH --nodes=1
 
@@ -19,7 +19,7 @@ MEMORYFILE=${basename}.txt
 NAME=Hector
 echo $NAME > $MEMORYFILE
 # Replace with your own variables. This is cpus-per-tasks partition mem
-echo 1 LM 1000GB >> $MEMORYFILE
+echo 1 LM 50GB >> $MEMORYFILE
 TIMELAPSES=30
 echo $TIMELAPSES >> $MEMORYFILE
 
