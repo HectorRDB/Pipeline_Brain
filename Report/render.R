29cd8f9a5ca7085e00ecad3326b65ddc7a67dc7d
Hector <hector.rouxdebezieux@berkeley.edu>
2019-10-21 12:16:59 +0200

    comp with allen

---
+++ Report/render.R
@@ -0,0 +1,7 @@
+for (dataset in c("SMARTer_cells_MOp", "SMARTer_nuclei_MOp")) {
+  rmarkdown::render(here("Report", 'ARI_comp.Rmd'),
+                    params = list(dataset = dataset,
+                                  title = paste0('Analysis of the ', dataset,
+                                                 ' dataset')),
+                    output_file = paste0(dataset, '_ARI_comp.html'))
+}
