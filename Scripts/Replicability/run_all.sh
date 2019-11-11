2516affe329177c746f6efd7e37475e9090708b4
Hector <hector.rouxdebezieux@berkeley.edu>
2019-11-11 13:58:59 -0800

    Update run_all.sh

--- Scripts/Replicability/run_all.sh
+++ Scripts/Replicability/run_all.sh
@@ -2,7 +2,7 @@
 #SBATCH --mail-user=hector.rouxdebezieux@berkeley.edu
 #SBATCH --mail-type=ALL
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=16
 #SBATCH --nodes=1
 
 MEMORYFILE=replicability.txt
