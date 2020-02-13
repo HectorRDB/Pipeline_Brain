2c690e2dde2bd013a0b484a7ee81a5881ee8a9ee
Hector <hector.rouxdebezieux@berkeley.edu>
2020-02-12 16:46:13 -0800

    tracking

--- Scripts/10X/1d-load_filter.sh
+++ Scripts/10X/1d-load_filter.sh
@@ -10,7 +10,17 @@
 loc="/pylon5/ib5phhp/hectorrb/10x_v3_cells_MOp"
 out="/pylon5/ib5phhp/hectorrb/ProcessedData/10x_v3_cells_MOp_filt.rds"
 
+timestamp=$(date +"%Y%m%d-%H%M%S")
+basename=1-load-filter_10x-v3-cells_${timestamp}
+MEMORYFILE=${basename}.txt
+
+echo $NAME > $MEMORYFILE
+echo 1 "RM" >> $MEMORYFILE
+TIMELAPSES=30
+echo $TIMELAPSES >> $MEMORYFILE
+
 module load intel/18.4
 module load hdf5
+module load gcc
 
 Rscript --no-save --verbose  1-load_filter.R -l $loc -o $out -c 30 > 1d.out 2>&1
