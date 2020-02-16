47214099d95356bbff77a66ae79397199824ad61
Hector <hector.rouxdebezieux@berkeley.edu>
2020-02-15 16:23:43 -0800

    Update 5c-monocle.sh

--- Scripts/10X/5c-monocle.sh
+++ Scripts/10X/5c-monocle.sh
@@ -6,10 +6,14 @@
 #SBATCH --nodes=1
 
 # This is not run on bridges but on the scf cluster
-loc="/scratch/users/singlecell/MiniAtlas/data/rds/10x_v3_nuclei_MOp_zinb.rds"
-out="/scratch/users/singlecell/MiniAtlas/data/rds/10x_v3_nuclei_MOp_monocle_all.csv"
-Rscript --verbose  5-monocle.R -l $loc -o $out > 5c.out 2>&1
+# For the 10x v3 we can use the smart-seq script
+Pipeline="/accounts/projects/epurdom/singlecell/allen/allen40K/Pipeline_Brain"
+
+loc="/scratch/users/singlecell/MiniAtlas/data/rds/10x_v3_nuclei_MOp_norm.rds"
+out=${Pipeline}/singleMethod/10x_v3_nuclei_MOp_Monocle.csv
+Rscript --verbose ${Pipeline}/Smart-Seq/6-monocle.R -l $loc -o $out > 5c.out 2>&1
 
 loc="/scratch/users/singlecell/MiniAtlas/data/rds/10x_v3_cells_MOp_zinb.rds"
-out="/scratch/users/singlecell/MiniAtlas/data/rds/10x_v3_cells_MOp_monocle_all.csv"
-Rscript --verbose  5-monocle.R -l $loc -o $out > 5d.out 2>&1
+out=${Pipeline}/singleMethod/10x_v3_cells_MOp_Monocle.csv
+Rscript --verbose ${Pipeline}/Smart-Seq/6-monocle.R -l $loc -o $out > 5d.out 2>&1
+
